24ab34ad814f169af21ff330ca107fa41b499a0c
adibrisan <adi.brisan@yahoo.com>
2022-11-18 21:04:17 +0200

    Updated skills section

--- components/custom/Skill.tsx
+++ components/custom/Skill.tsx
@@ -2,14 +2,17 @@ import { motion } from "framer-motion";
 
 type Props = {
   directionLeft?: boolean;
+  percentage: number;
+  language: string;
+  img: string;
 };
 
-const Skill = ({ directionLeft }: Props) => {
+const Skill = ({ directionLeft, percentage, language, img }: Props) => {
   return (
     <div className="group relative flex cursor-pointer">
       <motion.img
         initial={{
-          x: directionLeft ? -200 : 200,
+          x: directionLeft ? -50 : 50,
           opacity: 0,
         }}
         transition={{
@@ -17,13 +20,13 @@ const Skill = ({ directionLeft }: Props) => {
         }}
         whileInView={{ opacity: 1, x: 0 }}
         className="rounded-full border border-grey-200 object-cover w-24 h-24 2xlm:h-16 2xlm:w-16 xl:w-[80px] xl:h-[80px] filter group-hover:grayscale transition duration-400 ease-in-out"
-        src="https://cdn-icons-png.flaticon.com/128/5968/5968292.png"
+        src={img}
         alt="skill"
       />
       <div className="absolute h-24 w-24 lgm:h-16 lgm:w-16 opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white md:w-28 md:h-28 xl:w-[80px] xl:h-[80px] rounded-full z-0">
         <div className="h-full flex justify-center items-center">
-          <p className="text-sm font-bold text-black opacity-100">
-            100%<span className="block">JavaScript</span>
+          <p className="text-[10px] text-center font-bold text-black opacity-100">
+            {percentage}%<span className="block">{language}</span>
           </p>
         </div>
       </div>
--- components/layout/Skills.tsx
+++ components/layout/Skills.tsx
@@ -18,21 +18,57 @@ const Skills = (props: Props) => {
       <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm xlm:hidden">
         Hover over a skill to see level percentage
       </h3>
-      <div className="grid smm:grid-cols-3 sm:gap-5 grid-cols-4 gap-2 mb-[100px]">
-        <Skill directionLeft />
-        <Skill />
-        <Skill directionLeft />
-        <Skill />
-        <Skill directionLeft />
-        <Skill />
-        <Skill directionLeft />
-        <Skill />
-        <Skill directionLeft />
-        <Skill />
-        <Skill />
-        <Skill directionLeft />
-        <Skill />
-        <Skill directionLeft />
+      <div className="grid sm:gap-5 grid-cols-4 gap-2 mb-[100px]">
+        <Skill
+          percentage={80}
+          language="JavaScript"
+          img="https://cdn-icons-png.flaticon.com/128/5968/5968292.png"
+          directionLeft
+        />
+        <Skill
+          percentage={70}
+          language="TypeScript"
+          img="https://cdn-icons-png.flaticon.com/128/5968/5968381.png"
+        />
+        <Skill
+          percentage={90}
+          language="React"
+          img="https://cdn-icons-png.flaticon.com/128/753/753244.png"
+          directionLeft
+        />
+        <Skill
+          percentage={80}
+          language="SCSS"
+          img="https://cdn-icons-png.flaticon.com/128/919/919831.png"
+        />
+        <Skill
+          percentage={100}
+          language="HTML5"
+          img="https://cdn-icons-png.flaticon.com/128/174/174854.png"
+          directionLeft
+        />
+        <Skill
+          percentage={75}
+          language="CSS3"
+          img="https://cdn-icons-png.flaticon.com/128/732/732190.png"
+        />
+        <Skill
+          percentage={75}
+          language="styled-components"
+          img="https://cdn-icons-png.flaticon.com/128/599/599368.png"
+          directionLeft
+        />
+        <Skill
+          percentage={75}
+          language="NodeJS"
+          img="https://cdn-icons-png.flaticon.com/128/919/919825.png"
+        />
+        <Skill
+          percentage={75}
+          language="MongoDB"
+          img="https://miro.medium.com/max/512/1*doAg1_fMQKWFoub-6gwUiQ.png"
+          directionLeft
+        />
       </div>
     </motion.div>
   );
