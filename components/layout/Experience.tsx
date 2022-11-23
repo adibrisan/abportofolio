5b3befae0f0fee9fd898ea670ac0da33298296f1
adibrisan <adi.brisan@yahoo.com>
2022-11-23 14:16:35 +0200

    Exp title on small desktop screen

--- components/layout/Experience.tsx
+++ components/layout/Experience.tsx
@@ -17,7 +17,7 @@ const Experience = (props: Props) => {
       transition={{ duration: 2.5 }}
       className="relative px-10 h-screen flex flex-col justify-evenly items-center text-left md:flex-row max-w-full mx-auto overflow-hidden"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] smm:tracking-[1px] text-sm md:text-2xl text-cyan-200">
+      <h3 className="absolute top-24 expTitlePhone uppercase tracking-[20px] smm:tracking-[1px] text-sm md:text-2xl text-cyan-200">
         {t("common:experience")}
       </h3>
       <div className="w-full p-10 flex space-x-5 overflow-x-scroll snap-x snap-mandatory">
